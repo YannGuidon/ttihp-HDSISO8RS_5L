5c425a4835717aaaf1be2ba45f2c6eb67f50c497
YannGuidon <github@f-cpu.org>
2026-03-27 20:56:05 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -63,17 +63,17 @@ module tt_um_ygdes_hdsiso8_rs (
   // multiplexed output
   // assign uio_out = SHOW_LFSR ? LFSR_state8 : Decoded8 ;
   wire SHOW_LFSR_n1, SHOW_LFSR_n2;
-  (* keep *) sg13g2_inv_4 negShow1(.Y(SHOW_LFSR_n1), .A(SHOW_LFSR));
-  (* keep *) sg13g2_inv_4 negShow2(.Y(SHOW_LFSR_n2), .A(SHOW_LFSR));
+  (* keep *) sg13cmos5l_inv_4 negShow1(.Y(SHOW_LFSR_n1), .A(SHOW_LFSR));
+  (* keep *) sg13cmos5l_inv_4 negShow2(.Y(SHOW_LFSR_n2), .A(SHOW_LFSR));
   wire [7:0] LFSR_state8, Decoded8;
-  (* keep *) sg13g2_mux2_2 mux_uio0(.A0(LFSR_state8[0]), .A1(Decoded8[0]), .S(SHOW_LFSR_n1), .X(uio_out[0]));
-  (* keep *) sg13g2_mux2_2 mux_uio1(.A0(LFSR_state8[1]), .A1(Decoded8[1]), .S(SHOW_LFSR_n1), .X(uio_out[1]));
-  (* keep *) sg13g2_mux2_2 mux_uio2(.A0(LFSR_state8[2]), .A1(Decoded8[2]), .S(SHOW_LFSR_n1), .X(uio_out[2]));
-  (* keep *) sg13g2_mux2_2 mux_uio3(.A0(LFSR_state8[3]), .A1(Decoded8[3]), .S(SHOW_LFSR_n1), .X(uio_out[3]));
-  (* keep *) sg13g2_mux2_2 mux_uio4(.A0(LFSR_state8[4]), .A1(Decoded8[4]), .S(SHOW_LFSR_n2), .X(uio_out[4]));
-  (* keep *) sg13g2_mux2_2 mux_uio5(.A0(LFSR_state8[5]), .A1(Decoded8[5]), .S(SHOW_LFSR_n2), .X(uio_out[5]));
-  (* keep *) sg13g2_mux2_2 mux_uio6(.A0(LFSR_state8[6]), .A1(Decoded8[6]), .S(SHOW_LFSR_n2), .X(uio_out[6]));
-  (* keep *) sg13g2_mux2_2 mux_uio7(.A0(LFSR_state8[7]), .A1(Decoded8[7]), .S(SHOW_LFSR_n2), .X(uio_out[7]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio0(.A0(LFSR_state8[0]), .A1(Decoded8[0]), .S(SHOW_LFSR_n1), .X(uio_out[0]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio1(.A0(LFSR_state8[1]), .A1(Decoded8[1]), .S(SHOW_LFSR_n1), .X(uio_out[1]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio2(.A0(LFSR_state8[2]), .A1(Decoded8[2]), .S(SHOW_LFSR_n1), .X(uio_out[2]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio3(.A0(LFSR_state8[3]), .A1(Decoded8[3]), .S(SHOW_LFSR_n1), .X(uio_out[3]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio4(.A0(LFSR_state8[4]), .A1(Decoded8[4]), .S(SHOW_LFSR_n2), .X(uio_out[4]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio5(.A0(LFSR_state8[5]), .A1(Decoded8[5]), .S(SHOW_LFSR_n2), .X(uio_out[5]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio6(.A0(LFSR_state8[6]), .A1(Decoded8[6]), .S(SHOW_LFSR_n2), .X(uio_out[6]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio7(.A0(LFSR_state8[7]), .A1(Decoded8[7]), .S(SHOW_LFSR_n2), .X(uio_out[7]));
 
 
 ////////////////////////////// custom soup //////////////////////////////
@@ -81,19 +81,19 @@ module tt_um_ygdes_hdsiso8_rs (
   // select the clock
   // CLK_OUT = clk if CLK_SEL=0, else EXT_CLK
   // assign CLK_OUT = CLK_SEL ? EXT_CLK : clk;
-  (* keep *) sg13g2_mux2_2 mux_clk(.A0(clk), .A1(EXT_CLK), .S(CLK_SEL), .X(CLK_OUT));
+  (* keep *) sg13cmos5l_mux2_2 mux_clk(.A0(clk), .A1(EXT_CLK), .S(CLK_SEL), .X(CLK_OUT));
   // ring oscillator anyone ?
-  (* keep *) sg13g2_inv_4 negClkOut(.Y(CLK_OUTn), .A(CLK_OUT));
+  (* keep *) sg13cmos5l_inv_4 negClkOut(.Y(CLK_OUTn), .A(CLK_OUT));
 
   wire INT_RESET;
   // Combined and resynch'ed Reset
-  (* keep *) sg13g2_dfrbpq_2 DFF_reset(.Q(INT_RESET), .D(EXT_RST), .RESET_B(rst_n), .CLK(CLK_OUT));
+  (* keep *) sg13cmos5l_dfrbpq_2 DFF_reset(.Q(INT_RESET), .D(EXT_RST), .RESET_B(rst_n), .CLK(CLK_OUT));
 
 
   // Select + resynch D_in
   wire SISO_in;
   //      SISO_in <= DIN_SEL ? LFSR_BIT : D_IN;
-  (* keep *) sg13g2_sdfrbpq_1 sync_Din(.Q(SISO_in), .D(D_IN),
+  (* keep *) sg13cmos5l_sdfrbpq_1 sync_Din(.Q(SISO_in), .D(D_IN),
                          .SCD(LFSR_BIT), .SCE(DIN_SEL), .RESET_B(INT_RESET), .CLK(CLK_OUT));
 
 ////////////////////////////// sub-modules //////////////////////////////
@@ -112,29 +112,6 @@ module tt_um_ygdes_hdsiso8_rs (
     .DFF4(Johnson4),
     .Decoded8(Decoded8));
 
-/*
-// version : direct loopback, 23 cycles
-  wire [3:0] siso_start_even,   siso_start_odd,
-             siso_start_even_N, siso_start_odd_N,
-             latch4_even, latch4_odd;
-
-  siso_demux_mux_rs demux_mux(
-    .RESET(INT_RESET),
-    .CLK(CLK_OUT),
-    .Din(SISO_in),
-    .Latch8(Decoded8),
-    .Latch_even(latch4_even),
-    .Latch_odd(latch4_odd),
-    .siso_first_even(siso_start_even),
-    .siso_first_odd(siso_start_odd),
-    .siso_last_even(siso_start_even),
-    .siso_last_odd(siso_start_odd),
-    .siso_first_even_N(siso_start_even_N),
-    .siso_first_odd_N(siso_start_odd_N),
-    .siso_last_even_N(siso_start_even_N),
-    .siso_last_odd_N(siso_start_odd_N),
-    .Dout(D_OUT));
-*/
 
   wire [3:0]  latch4_even, latch4_odd,
     siso_start_even, siso_start_even_N, siso_start_odd, siso_start_odd_N,
@@ -187,244 +164,6 @@ module tt_um_ygdes_hdsiso8_rs (
     .siso_end_N(siso_end_odd_N),
     .latch4(latch4_odd)
   );
-
-/*
-  512 cells : OK !
-  siso_tranche4x4x4x4_rs_pos siso256_1(
-    .siso_start(siso_start_even),
-    .siso_start_N(siso_start_even_N),
-    .siso_end(siso_end_even),
-    .siso_end_N(siso_end_even_N),
-    .latch4(latch4_even)
-  );
-
-  siso_tranche4x4x4x4_rs_pos siso256_2(
-    .siso_start(siso_start_odd),
-    .siso_start_N(siso_start_odd_N),
-    .siso_end(siso_end_odd),
-    .siso_end_N(siso_end_odd_N),
-    .latch4(latch4_odd)
-  );
-*/
-
-/*
-  // .................................................
-  // 8×64: 384 cycles+23 = 407 cycles  OK!
-  wire [3:0]
-    latch4_even_N,   latch4_odd_N,
-    chain1_even,     chain1_even_N,     chain1_odd,     chain1_odd_N,
-    chain2_even,     chain2_even_N,     chain2_odd,     chain2_odd_N,
-    chain3_even,     chain3_even_N,     chain3_odd,     chain3_odd_N;
-  Inverters_x4 AmpE(.Y(latch4_even_N), .A(latch4_even));
-  Inverters_x4 AmpO(.Y(latch4_odd_N),  .A(latch4_odd));
-
-  siso_tranche4x4x4_rs_neg siso64_1(
-    .siso_in   (siso_start_even),
-    .siso_in_N (siso_start_even_N),
-    .siso_out  (chain1_even),
-    .siso_out_N(chain1_even_N),
-    .latch(latch4_even_N));
-
-  siso_tranche4x4x4_rs_neg siso64_2(
-    .siso_in   (siso_start_odd),
-    .siso_in_N (siso_start_odd_N),
-    .siso_out  (chain1_odd),
-    .siso_out_N(chain1_odd_N),
-    .latch(latch4_odd_N));
-
-  siso_tranche4x4x4_rs_neg siso64_3(
-    .siso_in   (chain1_even),
-    .siso_in_N (chain1_even_N),
-    .siso_out  (chain2_even),
-    .siso_out_N(chain2_even_N),
-    .latch(latch4_even_N));
-
-  siso_tranche4x4x4_rs_neg siso64_4(
-    .siso_in   (chain1_odd),
-    .siso_in_N (chain1_odd_N),
-    .siso_out  (chain2_odd),
-    .siso_out_N(chain2_odd_N),
-    .latch(latch4_odd_N));
-
-  siso_tranche4x4x4_rs_neg siso64_5(
-    .siso_in   (chain2_even),
-    .siso_in_N (chain2_even_N),
-    .siso_out  (chain3_even),
-    .siso_out_N(chain3_even_N),
-    .latch(latch4_even_N));
-
-  siso_tranche4x4x4_rs_neg siso64_6(
-    .siso_in   (chain2_odd),
-    .siso_in_N (chain2_odd_N),
-    .siso_out  (chain3_odd),
-    .siso_out_N(chain3_odd_N),
-    .latch(latch4_odd_N));
-    
-  siso_tranche4x4x4_rs_neg siso64_7(
-    .siso_in   (chain3_even),
-    .siso_in_N (chain3_even_N),
-    .siso_out  (siso_end_even),
-    .siso_out_N(siso_end_even_N),
-    .latch(latch4_even_N));
-
-  siso_tranche4x4x4_rs_neg siso64_8(
-    .siso_in   (chain3_odd),
-    .siso_in_N (chain3_odd_N),
-    .siso_out  (siso_end_odd),
-    .siso_out_N(siso_end_odd_N),
-    .latch(latch4_odd_N));
-*/
-
-/*
-  // .................................................
-  // 8×64: 384 cycles+23 = 407 cycles = OK with clock period=30
-  wire [3:0]
-    chain1_even,     chain1_even_N,     chain1_odd,     chain1_odd_N,
-    chain2_even,     chain2_even_N,     chain2_odd,     chain2_odd_N,
-    chain3_even,     chain3_even_N,     chain3_odd,     chain3_odd_N;
-
-  siso_tranche4x4x4_rs_pos siso64_1(
-    .siso_in   (siso_start_even),
-    .siso_in_N (siso_start_even_N),
-    .siso_out  (chain1_even),
-    .siso_out_N(chain1_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_2(
-    .siso_in   (siso_start_odd),
-    .siso_in_N (siso_start_odd_N),
-    .siso_out  (chain1_odd),
-    .siso_out_N(chain1_odd_N),
-    .latch(latch4_odd));
-
-  siso_tranche4x4x4_rs_pos siso64_3(
-    .siso_in   (chain1_even),
-    .siso_in_N (chain1_even_N),
-    .siso_out  (chain2_even),
-    .siso_out_N(chain2_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_4(
-    .siso_in   (chain1_odd),
-    .siso_in_N (chain1_odd_N),
-    .siso_out  (chain2_odd),
-    .siso_out_N(chain2_odd_N),
-    .latch(latch4_odd));
-
-  siso_tranche4x4x4_rs_pos siso64_5(
-    .siso_in   (chain2_even),
-    .siso_in_N (chain2_even_N),
-    .siso_out  (chain3_even),
-    .siso_out_N(chain3_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_6(
-    .siso_in   (chain2_odd),
-    .siso_in_N (chain2_odd_N),
-    .siso_out  (chain3_odd),
-    .siso_out_N(chain3_odd_N),
-    .latch(latch4_odd));
-    
-  siso_tranche4x4x4_rs_pos siso64_7(
-    .siso_in   (chain3_even),
-    .siso_in_N (chain3_even_N),
-    .siso_out  (siso_end_even),
-    .siso_out_N(siso_end_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_8(
-    .siso_in   (chain3_odd),
-    .siso_in_N (chain3_odd_N),
-    .siso_out  (siso_end_odd),
-    .siso_out_N(siso_end_odd_N),
-    .latch(latch4_odd));
-*/
-
-/*
-  // .................................................
-  wire [3:0]
-    chain1_even,     chain1_even_N,     chain1_odd,     chain1_odd_N,
-    chain2_even,     chain2_even_N,     chain2_odd,     chain2_odd_N;
-
-  // 6×64: 288 cycles+23 = 311 cycles OK
-  siso_tranche4x4x4_rs_pos siso64_1(
-    .siso_in   (siso_start_even),
-    .siso_in_N (siso_start_even_N),
-    .siso_out  (chain1_even),
-    .siso_out_N(chain1_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_2(
-    .siso_in   (siso_start_odd),
-    .siso_in_N (siso_start_odd_N),
-    .siso_out  (chain1_odd),
-    .siso_out_N(chain1_odd_N),
-    .latch(latch4_odd));
-
-  siso_tranche4x4x4_rs_pos siso64_3(
-    .siso_in   (chain1_even),
-    .siso_in_N (chain1_even_N),
-    .siso_out  (chain2_even),
-    .siso_out_N(chain2_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_4(
-    .siso_in   (chain1_odd),
-    .siso_in_N (chain1_odd_N),
-    .siso_out  (chain2_odd),
-    .siso_out_N(chain2_odd_N),
-    .latch(latch4_odd));
-
-  siso_tranche4x4x4_rs_pos siso64_5(
-    .siso_in   (chain2_even),
-    .siso_in_N (chain2_even_N),
-    .siso_out  (siso_end_even),
-    .siso_out_N(siso_end_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_6(
-    .siso_in   (chain2_odd),
-    .siso_in_N (chain2_odd_N),
-    .siso_out  (siso_end_odd),
-    .siso_out_N(siso_end_odd_N),
-    .latch(latch4_odd));
-*/
-
-/*
-  // .................................................
-  // 4×64: sim OK / 256 registers = 192 cycles
-  wire [3:0]
-    chain_even,     chain_even_N,     chain_odd,     chain_odd_N;
-
-  siso_tranche4x4x4_rs_pos siso64_1(
-    .siso_in   (siso_start_even),
-    .siso_in_N (siso_start_even_N),
-    .siso_out  (chain_even),
-    .siso_out_N(chain_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_2(
-    .siso_in   (siso_start_odd),
-    .siso_in_N (siso_start_odd_N),
-    .siso_out  (chain_odd),
-    .siso_out_N(chain_odd_N),
-    .latch(latch4_odd));
-
-  siso_tranche4x4x4_rs_pos siso64_3(
-    .siso_in   (chain_even),
-    .siso_in_N (chain_even_N),
-    .siso_out  (siso_end_even),
-    .siso_out_N(siso_end_even_N),
-    .latch(latch4_even));
-
-  siso_tranche4x4x4_rs_pos siso64_4(
-    .siso_in   (chain_odd),
-    .siso_in_N (chain_odd_N),
-    .siso_out  (siso_end_odd),
-    .siso_out_N(siso_end_odd_N),
-    .latch(latch4_odd));
-*/
-
 
 ////////////////////////////// All the dummies go here //////////////////////////////
 
